c068cdfeee912ee23713331d2c4404ff3c6e2fca
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-06-20 00:23:30 +0530

    basic UI added

---
+++ clientLogic.js
@@ -0,0 +1,53 @@
+function incrementVeg() {
+    let counterElement = document.getElementById("counter");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount + 1;
+  }
+  
+  function decrementVeg() {
+    let counterElement = document.getElementById("counter");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount - 1;
+  }
+
+
+
+function incrementNonVeg() {
+    let counterElement = document.getElementById("lol");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount + 1;
+  }
+  
+  function decrementNonVeg() {
+    let counterElement = document.getElementById("lol");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount - 1;
+  }
+
+
+
+function incrementChappati() {
+    let counterElement = document.getElementById("dem");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount + 1;
+  }
+  
+  function decrementChappati() {
+    let counterElement = document.getElementById("dem");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount - 1;
+  }
+
+
+
+function incrementRice() {
+    let counterElement = document.getElementById("broo");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount + 1;
+  }
+  
+  function decrementRice() {
+    let counterElement = document.getElementById("broo");
+    let currentCount = parseInt(counterElement.innerHTML);
+    counterElement.innerHTML = currentCount - 1;
+  }
