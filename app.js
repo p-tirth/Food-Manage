415b0235b5012ac040a05aaead106be86a09721a
Tirth <tirthpatel2992003@gamail.com>
2023-06-20 13:30:01 +0530

    python executed with js

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.post("/submit-data", (req, res) => {
   const data = req.body;
   fs.writeFile("data.json", JSON.stringify(data), (err) => {
     if (err) throw err;
-    console.log("Data saved to data.json");
+    const { PythonShell } = require('python-shell');
+    console.log("shell")
+    PythonShell.run('client.py', null, function (err) {
+    if (err){throw err}else{console.log('Python script executed successfully.');}
+});
+
   });
   res.sendStatus(200);
 });
